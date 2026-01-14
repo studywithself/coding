c13d78e5630c8df4821fa0d01fbccfc13ea28474
StudyWithSelf <limuisoon@gmail.com>
2026-01-14 21:47:01 +0900

    Title: 문자열 범위 출력하기 2, Time: 124ms, Memory: 10MB, Status: Passed - Codetree

--- 260114/문자열 범위 출력하기 2/print-string-in-range-2.java
+++ 260114/문자열 범위 출력하기 2/print-string-in-range-2.java
@@ -13,7 +13,7 @@ public class Main {
             }
         } else {
             for(int i = str.length()-1; i >= 0; i--)
-                System.out.println(str.charAt(i));
+                System.out.print(str.charAt(i));
         }
         
     }
