fc9bc3dfbb569af3ad5b4979e26a8bdf0b597997
StudyWithSelf <limuisoon@gmail.com>
2026-01-18 22:38:59 +0900

    Title: 문자 제거하기, Time: 132ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 260118/문자 제거하기/del-char.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String input = sc.next();
+        String result = input;
+
+        for (int i=0; i<input.length() - 1; i++) {
+            int position = sc.nextInt();
+
+            if (input.length() > position) {
+                result = result.substring(0, position) + result.substring(position + 1);
+                System.out.println(result);
+            } else {
+                result = result.substring(0, result.length()-1);
+                System.out.println(result);
+            }
+
+            if (result.length() <= 1) {
+                break;
+            }
+        }
+    }
+}
